a3d666c047376d7b3ab82fbf6aeef65208e10358
MQ29 <matiax2829@gmail.com>
2023-10-21 21:28:28 +0200

    Dodaj pliki projektów.

---
+++ BookLibrary/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using BookLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibrary.Controllers
+{
+    public class BookController : Controller
+    {
+        static readonly Dictionary<int, Book> _books = new Dictionary<int, Book>();
+        static int id = 1;
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if(ModelState.IsValid) 
+            {
+                book.Id = id++;
+                _books.Add(book.Id, book);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ BookLibrary/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace BookLibrary.Models
+{
+    public class Book
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [Required(ErrorMessage = "You have to type ISBN number.")]
+        public string ISBN { get; set; }
+        [Range(0, 2023)]
+        public int? PublicationDate { get; set; } 
+        public string? Publisher { get; set; }
+    }
+}
